2fb04ead6c42ab6db0d6b0ce9f36c60e401a83bc
hannabacave <hannabacave@gmail.com>
2020-04-18 01:11:47 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -71,6 +71,7 @@ pygments_style = 'manni'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_drove_theme'
+html_them_path = 'C:/Users/hbaca/Anaconda3/Lib/site-packages/sphinx_drove_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
