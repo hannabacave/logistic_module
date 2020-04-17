6cf5c7ff8b3412fa3ee64e4e000040cd39a947db
Mimouni Yassine <mimouni.yassine84@gmail.com>
2020-04-17 20:41:09 +0200

    update Mandelbrot3D.py and notebook

--- logistic_module/Mandelbrot3D/Mandelbrot3D.py
+++ logistic_module/Mandelbrot3D/Mandelbrot3D.py
@@ -25,11 +25,33 @@ for k in range(1,n+1):
     Z=ZZ
     W= f(Z)-0.625    
 
-    
-fig = go.Figure(data=[go.Surface(z=W, x=X, y=Y,colorscale = 'Reds', showscale = False),
-                      go.Surface(z=-W,x=X, y=Y, colorscale = 'Reds',reversescale = True, showscale = False)],
+ 
+fig = go.Figure(data = [go.Surface(z=W, x=X, y=Y,
+                                   colorscale = 'Reds',
+                                   showscale = False,
+                                   hoverinfo = "none",
+                                   contours = dict(x = dict(highlight = False),
+                                                   y = dict(highlight = False),
+                                                   z = dict(highlight = False))),
+                        go.Surface(z=-W,x=X, y=Y,
+                                   colorscale = 'Reds',
+                                   reversescale = True,
+                                   showscale = False,
+                                   hoverinfo = "none",
+                                   showlegend = False)],                       
                 
-               layout=go.Layout(title='Mandelbrot 3D visualization'))
-
+               layout = go.Layout(title='Mandelbrot 3D visualization',
+                                  width = 700,
+                                  height = 700,
+                                  hovermode= 'closest',
+                                  template = "plotly_dark",
+                                  scene = dict(xaxis = dict(visible = False, spikesides = False),
+                                               yaxis = dict(visible = False),
+                                               zaxis = dict(visible = False),
+                                               hovermode = False,
+                                               camera = dict(eye=dict(x=0, y=-0.8, z=2.5),
+                                                             center=dict(x=0, y=0, z=0)) 
+                                               
+                                             )))
+                                                    
 plotly.offline.plot(fig, filename='Mandelbrot3D.html')
-#fig.show()
