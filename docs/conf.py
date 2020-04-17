c1c6f2c7220f0af593872002332268501d8aa66f
hannabacave <hannabacave@gmail.com>
2020-04-17 23:51:14 +0200

    change doc on docs

--- docs/conf.py
+++ docs/conf.py
@@ -33,16 +33,9 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
-autodoc_mock_imports = ['sip', 'PyQt5']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
