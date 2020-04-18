f596b3323c04801aa06bfadcf16cb588e5d1104d
hannabacave <hannabacave@gmail.com>
2020-04-18 15:55:37 +0200

    ajout d'un test sur Mandelbrot2D
    Co-Authored-By: cindy-dotcom <cindy-dotcom@users.noreply.github.com>

--- logistic_module/tests/test_logistic_module.py
+++ logistic_module/tests/test_logistic_module.py
@@ -1,8 +1,13 @@
 from logistic_module.LogisticEquation.Logistic_and_vectorization import Logistic_equation
+from logistic_module.Mandelbrot2D.Mandelbrot2D import Mandelbrot_2D
 le = Logistic_equation(n=1)
+m2 = Mandelbrot_2D(hauteur=2, largeur=2)
 
 def test_logistic():
     assert le.logistic(2, 1) == 0
 
 def test_vectorization():
     assert le.vectorization(1,2) == [(1, 0), (1, 0), (0, 0)]
+
+def test_mandelbrotset():
+    assert m2.__Mandelbrotset__(4, 1, 2, 1, 2) == [[0., 1.], [0., 1.]]
