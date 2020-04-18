d12623510c2c7b3cc905b9130b91b975865596ef
Cindy DELAGE <cindydelage61@gmail.com>
2020-04-18 17:52:02 +0200

    Changes in script.py, Mandelbrot2D.py and Mandelbrot2DAnimation.py

--- logistic_module/Mandelbrot2D/Mandelbrot2DAnimation.py
+++ logistic_module/Mandelbrot2D/Mandelbrot2DAnimation.py
@@ -1,6 +1,20 @@
-import time
+from numba import jit 
+import matplotlib.pyplot as plt
+from Mandelbrot2D import Mandelbrot_2D
 
-start=time.time()
+Z_2 = Mandelbrot_2D(500, 500, 50, -1.5, 0.2, -0.85, 0.85)
+Z_3 = Mandelbrot_2D(500, 500, 50, -1.4, 0, -0.9, 0.5)
+Z_4 = Mandelbrot_2D(500, 500, 50, -1.4, -0.2, -0.9, 0.3)
+Z_5 = Mandelbrot_2D(500, 500, 50, -1.4, -0.5, -0.7, 0.2)
+Z_6 = Mandelbrot_2D(500, 500, 50, -1.4, -0.9, -0.5, 0.1)
+Z_7 = Mandelbrot_2D(500, 500, 50, -1.4, -1.2, -0.13, 0.07)
+Z_8 = Mandelbrot_2D(500, 500, 50, -1.4, -1.23, -0.10, 0.07)
+Z_9 = Mandelbrot_2D(500, 500, 50, -1.4, -1.25, -0.08, 0.07)
+Z_10 = Mandelbrot_2D(500, 500, 50, -1.4, -1.3, -0.06, 0.04)
+Z_11 = Mandelbrot_2D(500, 500, 50, -1.4, -1.32, -0.04, 0.04)
+Z_12 = Mandelbrot_2D(500, 500, 50, -1.4, -1.36, -0.04, 0.04)
+
+@jit(nopython=False)
 def animate(i):
     """Animated zoom on the mandelbrot set
     :param i: the number of time the functions is to be used
@@ -11,29 +25,27 @@ def animate(i):
     """
     #The programm passes this function multiples times: it goes from i=0 to a value of i that we are going to chose in the document script.py. For each value of i, we print a different matrice. As i increases, the interval for min values and max values (for x and y) becomes smaller, visuallly it is like if we were zooming on the first picture (obtained by printing Z)
     if i == 0:
-        im = plt.imshow(Z_2)
+        im = plt.imshow(Z_2.__Mandelbrotset__)
     if i == 1:
-        im = plt.imshow(Z_3)
+        im = plt.imshow(Z_3.__Mandelbrotset__)
     if i == 2:
-        im = plt.imshow(Z_4)
+        im = plt.imshow(Z_4.__Mandelbrotset__)
     if i == 3:
-        im = plt.imshow(Z_5)
+        im = plt.imshow(Z_5.__Mandelbrotset__)
     if i == 4:
-        im = plt.imshow(Z_6)
+        im = plt.imshow(Z_6.__Mandelbrotset__)
     if i == 5:
-        im = plt.imshow(Z_7)
+        im = plt.imshow(Z_7.__Mandelbrotset__)
     if i == 6:
-        im = plt.imshow(Z_8)
+        im = plt.imshow(Z_8.__Mandelbrotset__)
     if i == 7:
-        im = plt.imshow(Z_9)
+        im = plt.imshow(Z_9.__Mandelbrotset__)
     if i == 8:
-        im = plt.imshow(Z_10)
+        im = plt.imshow(Z_10.__Mandelbrotset__)
     if i == 9:
-        im = plt.imshow(Z_11)
+        im = plt.imshow(Z_11.__Mandelbrotset__)
     if i == 10:
-        im = plt.imshow(Z_12)
+        im = plt.imshow(Z_12.__Mandelbrotset__)
     return im,
-end=time.time()
-print("Temps passé pour éxécuter la fonction:  {0:.5f} s.".format(end - start))
 
 
--- logistic_module/Mandelbrot2D/script.py
+++ logistic_module/Mandelbrot2D/script.py
@@ -1,42 +1,40 @@
 #The functions used in this script are coming from other scripts, so we have to import them:
-from Mandelbrot2D import *
-from Mandelbrot2DAnimation import *
-
+from Mandelbrot2D import Mandelbrot_2D
+from Mandelbrot2DAnimation import animate
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
+from numba import int64
+from numba import float64
+from numba import jitclass
+from numba import jit 
+import time
+
+start2D = time.time()
+
 #This values of parameters allow to print the Mandelbrot set like we often represent it:
 Z = Mandelbrot_2D(hauteur=500, largeur=500, max_iteration=100,
                   xmin=-2, xmax=0.5, ymin=-1.25, ymax=1.25)
-Z.__Mandelbrotset__(500, 500, 100,  -2, 0.5, -1.25, 1.25)
 
-from matplotlib import animation
 fig = plt.figure()
 fig.suptitle("Ensemble de mandelbrot")
-im = plt.imshow(Z.__Mandelbrotset__(500, 500, 100,  -2, 0.5, -1.25, 1.25))
+im = plt.imshow(Z.__Mandelbrotset__)
 plt.colorbar()
 plt.show()
 
-#We can see that the shape obtained by printing Z can be seen again by zooming (Mandelbrot set is a fractale set). Changing the values for xmin, xmax, ymin et ymax (by making the interval between min and max values smaller) is in fact like making a zoom on the Mandelbrot set. 
-Z_1 = mandelbrot(500, 500, 50, -2, 0.5, -1.25, 1.25)
-Z_2 = mandelbrot(500, 500, 50, -1.5, 0.2, -0.85, 0.85)
-Z_3 = mandelbrot(500, 500, 50, -1.4, 0, -0.9, 0.5)
-Z_4 = mandelbrot(500, 500, 50, -1.4, -0.2, -0.9, 0.3)
-Z_5 = mandelbrot(500, 500, 50, -1.4, -0.5, -0.7, 0.2)
-Z_6 = mandelbrot(500, 500, 50, -1.4, -0.9, -0.5, 0.1)
-Z_7 = mandelbrot(500, 500, 50, -1.4, -1.2, -0.13, 0.07)
-Z_8 = mandelbrot(500, 500, 50, -1.4, -1.23, -0.10, 0.07)
-Z_9 = mandelbrot(500, 500, 50, -1.4, -1.25, -0.08, 0.07)
-Z_10 = mandelbrot(500, 500, 50, -1.4, -1.3, -0.06, 0.04)
-Z_11 = mandelbrot(500, 500, 50, -1.4, -1.32, -0.04, 0.04)
-Z_12 = mandelbrot(500, 500, 50, -1.4, -1.36, -0.04, 0.04)
+end2D=time.time()
+print("Time spent to print the Mandelbrotset:  {0:.5f} s.".format(end2D - start2D))
 
+start_animation=time.time()
+#We can see that the shape obtained by printing Z can be seen again by zooming (Mandelbrot set is a fractale set). Changing the values for xmin, xmax, ymin et ymax (by making the interval between min and max values smaller) is in fact like making a zoom on the Mandelbrot set. 
 fig, ax = plt.subplots(1, 1, figsize=(12, 6),
                        sharey=True)
+                       
+Z_1 = Mandelbrot_2D(500, 500, 50, -2, 0.5, -1.25, 1.25)
 #We print the original picture of the Mandelbrot set, without any zoom:
-im = plt.imshow(Z_1)
+im = plt.imshow(Z_1.__Mandelbrotset__)
 #We use the function Animation from matplotlib so that every zoom is printed after the previous one. Tha values for i are the integers between 0 and 11, it corresponds to frames in the parameters:
 anim = animation.FuncAnimation(fig, animate, frames=np.arange(11), interval=400, blit=False)
 #We save the animation:
@@ -44,3 +42,6 @@ anim.save('MandelbrotAnimation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 plt.show()
 fig.suptitle("Fractale de mandelbrot")
 plt.colorbar()
+
+end_animation=time.time()
+print("Time spent to animate the zoom on the Mandelbrotset:  {0:.5f} s.".format(end_animation - start_animation))
