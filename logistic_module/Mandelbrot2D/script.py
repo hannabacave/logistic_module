d36b5a64c5e5b4bd0898a0b9b52336f12e80681b
Cindy DELAGE <cindydelage61@gmail.com>
2020-04-19 16:20:54 +0200

    Changed the animation

--- logistic_module/Mandelbrot2D/script.py
+++ logistic_module/Mandelbrot2D/script.py
@@ -19,7 +19,7 @@ Z = Mandelbrot_2D(hauteur=500, largeur=500, max_iteration=100,
                   xmin=-2, xmax=0.5, ymin=-1.25, ymax=1.25)
 
 fig = plt.figure()
-fig.suptitle("Ensemble de mandelbrot")
+fig.suptitle("Mandelbrot set")
 im = plt.imshow(Z.__Mandelbrotset__)
 plt.colorbar()
 plt.show()
@@ -31,17 +31,15 @@ start_animation=time.time()
 #We can see that the shape obtained by printing Z can be seen again by zooming (Mandelbrot set is a fractale set). Changing the values for xmin, xmax, ymin et ymax (by making the interval between min and max values smaller) is in fact like making a zoom on the Mandelbrot set. 
 fig, ax = plt.subplots(1, 1, figsize=(12, 6),
                        sharey=True)
-                       
+fig.suptitle("Mandelbrot fractal")
 Z_1 = Mandelbrot_2D(500, 500, 50, -2, 0.5, -1.25, 1.25)
 #We print the original picture of the Mandelbrot set, without any zoom:
 im = plt.imshow(Z_1.__Mandelbrotset__)
 #We use the function Animation from matplotlib so that every zoom is printed after the previous one. Tha values for i are the integers between 0 and 11, it corresponds to frames in the parameters:
 anim = animation.FuncAnimation(fig, animate, frames=np.arange(11), interval=400, blit=False)
-#We save the animation:
-anim.save('MandelbrotAnimation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
 plt.show()
-fig.suptitle("Fractale de mandelbrot")
 plt.colorbar()
+#We save the animation:
 
 end_animation=time.time()
 print("Time spent to animate the zoom on the Mandelbrotset:  {0:.5f} s.".format(end_animation - start_animation))
