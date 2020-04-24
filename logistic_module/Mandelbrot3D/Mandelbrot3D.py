3770d63ec756306d4221780c03eceddd41e8c7c1
Mimouni Yassine <mimouni.yassine84@gmail.com>
2020-04-24 15:38:06 +0200

    modifs script

--- logistic_module/Mandelbrot3D/Mandelbrot3D.py
+++ logistic_module/Mandelbrot3D/Mandelbrot3D.py
@@ -1,13 +1,16 @@
 import numpy as np
-
+import plotly
+import plotly.graph_objects as go
 
 class Mand_3D():
     
-    """Class containing two functions to plot the Mandelbrot set in 3D with X,Y and Z axis following :  
+    """Class containing two functions to plot the Mandelbrot set in 3D and one to make it interactive :  
         
-         - Square area observed formed by X and Y axis
+         1) Create square area observed formed by X and Y axis
          
-         - Matrix of elevation value of the Mandelbrot set in 2D as Z parameter"""
+         2) Matrix of elevation value of the Mandelbrot set in 2D as Z parameter
+         
+         3) Use the two later points to make an interactive vizualisation of the Mandelbrot set"""
     
     
     def __init__(self,n,M,L,dx,dy):
@@ -64,9 +67,9 @@ class Mand_3D():
             1) Initialize the elements of the Mandelbrot equation
             2) Compute n iteration of the Mandelbrot equation on a (M,M) shaped 2D-array  
             3) Apply an elevation function on this 2D-array
-            4) Filter the element of the 2D-array
+            4) Filter the element where we apply the elevation function
             
-           Return a 2D-array with the elevated 2D Mandelbrot set."""
+           Return the elevated 2D Mandelbrot set as a 2D-array."""
         
         X,Y = self.grid()
         C = X + 1j*Y   
@@ -80,3 +83,51 @@ class Mand_3D():
         W[W>0] = W[W>0]*0.0001
         
         return W
+
+    
+    def interact(self):
+    
+        """Return an interactive representation of the Mandelbrot set in 3D using plotly."""
+
+        fig = go.Figure(data = [go.Surface(z=self.mand(), x=self.grid()[0], y=self.grid()[1],                                   
+                                           colorscale = 'Reds', 
+                                           showscale = False,
+                                           connectgaps = False,
+                                           hoverinfo = "none",
+                                           contours = dict(x = dict(highlight = False),
+                                                           y = dict(highlight = False,
+                                                                    show = True,
+                                                                    start = -2,
+                                                                    end = 2,
+                                                                    size = 1,
+                                                                    color = "yellow"),
+                                                           z = dict(highlight = False))), 
+
+                                go.Surface(z=-self.mand(), x=self.grid()[0], y=self.grid()[1],
+                                           colorscale = 'Reds',
+                                           reversescale = True,
+                                           showscale = False,
+                                           connectgaps = False,
+                                           hoverinfo = "none",
+                                           showlegend = False,
+                                           contours = dict(x = dict(highlight = False),
+                                                           y = dict(highlight = False,
+                                                                    show = True,
+                                                                    start = -2,
+                                                                    end = 2,
+                                                                    size = 1,
+                                                                    color = "yellow"),
+                                                           z = dict(highlight = False)))],                       
+                
+                                layout = go.Layout(title='Mandelbrot 3D interactive visualization',
+                                                   width = 600,
+                                                   height = 600,
+                                                   hovermode= 'closest',
+                                                   template = "plotly_dark",
+                                                   scene = dict(xaxis = dict(visible = False),
+                                                                yaxis = dict(visible = False),
+                                                                zaxis = dict(visible = False),
+                                                                hovermode = False,
+                                                                camera = dict(eye=dict(x=0, y=-0.8, z=2.5) ))))
+    
+        return fig
