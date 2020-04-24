ed859e610f2be7c24b4aa61d651743816e6f8fb0
Cindy DELAGE <cindydelage61@gmail.com>
2020-04-24 17:57:02 +0200

    Add the possibility of saving each of 200 frames from the animation

--- logistic_module/Mandelbrot2D/Inside_Mandelbrotset.py
+++ logistic_module/Mandelbrot2D/Inside_Mandelbrotset.py
@@ -1,9 +1,13 @@
+import os 
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import animation
+from numba import jit 
+from memory_profiler import profile
 
 from Mandelbrot2D import Mandelbrot_2D
 
+@profile
 def Inside_the_set():
     """Animate a zoom inside the Mandelbrot set
     :return: video of more than 200 frames
@@ -13,7 +17,11 @@ def Inside_the_set():
     fig.suptitle("Inside the Mandelbrot fractal")
     Z=Mandelbrot_2D(500, 500, 50, -2, 0.5, -1.25, 1.25)
     im = plt.imshow(Z.Mandelbrotset)
-    def animate_200(i):
+    #Possibility to save each frames in a temp directory, created on the fly
+    x=input("Do you want to save each frame of the animation in a folder ? This will slow the animation. Answer 'yes' or 'no' without quotes :")
+    if x=='yes':
+        os.mkdir('temp')
+    def animate_200(i, x):
         if i<=50:
             Z=Mandelbrot_2D(500,500,50,-2+0.02*i,0.5-0.02*i, -1.25+0.02*i, 1.25-0.02*i)
             im.set_data(Z.Mandelbrotset)
@@ -23,6 +31,12 @@ def Inside_the_set():
         if i>130:
             Z_2=Mandelbrot_2D(500,500,600,-0.74+0.00044*(i-150),-0.71-0.00008*(i-150), 0.19+0.00046*(i-150), 0.22-0.00006*(i-150))
             im.set_data(Z_2.Mandelbrotset)
+        if x=='yes':
+            plt.savefig('temp/frame_{}.png'.format(i))
         return im,
-    anim = animation.FuncAnimation(fig, animate_200, frames=np.arange(0,210,1), interval=1, blit=False)
+    anim = animation.FuncAnimation(fig, animate_200, frames=np.arange(0,210,1), fargs=(x,), interval=1, blit=False)
     plt.show()
+
+Inside_the_set()
+
+
