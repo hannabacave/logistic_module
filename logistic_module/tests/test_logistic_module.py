9000781c3394f21e0d8bc15a3839375a8a256ef2
Cindy-dotcom1 <60815796+Cindy-dotcom1@users.noreply.github.com>
2020-04-28 17:51:38 +0200

    Update test

--- logistic_module/tests/test_logistic_module.py
+++ logistic_module/tests/test_logistic_module.py
@@ -1,7 +1,7 @@
 from logistic_module.LogisticEquation.Logistic_and_vectorization import Logistic_equation
 from logistic_module.Mandelbrot2D.Mandelbrot_without_numba.Mandelbrot2D_without_numba import Mandelbrot_2D
 from logistic_module.Mandelbrot2D.Mandelbrot_without_numba.Mandelbrot2DAnimation_without_numba import animate
-from logistic_module.Mandelbrot2D.Mandelbrot_without_numba.Inside_Mandelbrotset_without_numba import Inside_the_set
+from logistic_module.Mandelbrot2D.Mandelbrot_without_numba.Inside_Mandelbrotset_without_numba import animate_200
 from logistic_module.Mandelbrot2D.Mandelbrot_without_numba.Plot_pattern_without_numba import plot_patterns
 from logistic_module.Mandelbrot3D.Mandelbrot3D import Mand_3D
 from scipy.sparse import csr_matrix, isspmatrix
@@ -10,6 +10,11 @@ import numpy as np
 
 le = Logistic_equation(n=1)
 m2 = Mandelbrot_2D(largeur=2,hauteur=2,max_iteration=4,xmin=1, xmax=2, ymin=1, ymax=2)
+Z=Mandelbrot_2D(500,500,5,-1,1,-1,1)
+Z_1=Mandelbrot_2D(500,500,50,-1, -0.5, -0.25, 0.25)
+Z_2=Mandelbrot_2D(500,500,50,-0.74,-0.71,0.19,0.22)
+Z_3=Mandelbrot_2D(500,500,50,0.25,0.30,-0.01,0.04)
+Z_4=Mandelbrot_2D(500,500,50,-0.057,-0.075,0.6435,0.6544)
 m3 = Mand_3D(n=200, M=200, L=1.4, dx=-0.6, dy=0.0)
 
 
@@ -24,7 +29,6 @@ def test_mandelbrotset():
     assert np.any(m2.Mandelbrotset() == np.array([[1., 1.], [1., 1.]]))
 
 def Mandelbrot_2D_fig():
-    Z=Mandelbrot_2D(500,500,5,-1,1,-1,1)
     assert Z.fig.shape==(500,500)
     
 def test_plot_patterns():
@@ -34,20 +38,20 @@ def test_animation_2D():
     im=animate(0)
     assert str(type(im))=="<class 'matplotlib.image.AxesImage'>"
 
+def test_animation_200():
+    im=animate_200(0,"no")
+    assert str(type(im))=="<class 'tuple'>"
+
 def test_sparse_matrix_1():
-    Z=Mandelbrot_2D(500,500,50,-1, -0.5, -0.25, 0.25)
     assert isspmatrix(csr_matrix(Z.Mandelbrotset()))==True
 
 def test_sparse_matrix_2():
-    Z_1=Mandelbrot_2D(500,500,50,-0.74,-0.71,0.19,0.22)
     assert isspmatrix(csr_matrix(Z_1.Mandelbrotset()))==True
 
 def test_sparse_matrix_3():
-    Z_2=Mandelbrot_2D(500,500,50,0.25,0.30,-0.01,0.04)
     assert isspmatrix(csr_matrix(Z_2.Mandelbrotset()))==True
 
 def test_sparse_matrix_4():
-    Z=Mandelbrot_2D(500,500,50,-0.057,-0.075,0.6435,0.6544)
     assert isspmatrix(csr_matrix(Z.Mandelbrotset()))==True
     
 
