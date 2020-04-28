d512bdcfa5ead95af1e11691edbb51dae92f379a
Cindy-dotcom1 <60815796+Cindy-dotcom1@users.noreply.github.com>
2020-04-28 20:08:58 +0200

    Update Mandelbrot2D.py

--- logistic_module/Mandelbrot2D/Mandelbrot2D.py
+++ logistic_module/Mandelbrot2D/Mandelbrot2D.py
@@ -13,7 +13,7 @@ spec = [('largeur', int64),
 
 @jitclass(spec)
 class Mandelbrot_2D:
-    """Returns a matrix with values 0 if the point is in the Mandelbrot set, 1 if it is not. The number of iterations can be chosen with maxiteration, so that the function return the state of the Mandelbrit set after this number of iterations
+    """Returns a matrix with values 0 if the point is in the Mandelbrot set, 1 if it is not. The number of iterations can be chosen with maxiteration, so that the function return the state of the Mandelbrot set after this number of iterations
     
     :return: A matrix with values 1 (if the point is not in the mandelbrot set) and 0 (if the point is in the Mandlebrot set)
     :rtype: array of integers
@@ -66,26 +66,19 @@ class Mandelbrot_2D:
            :return: the state of the Mandelbrot set at the iteration max_iteration
            :return type:array of integers 
         """
-        #We first create a matrix with dimensions chosen by the user with the parameters "largeur" and "hauteur". Shes only contains zeros, but some of the values of the matrix will be changed by the function
         self.array=self.fig
         for x in range(self.hauteur):
-            #for each line of the matrix, we define the value cx, that will be used later to change the values of (xn)
             cx = (x * (self.xmax - self.xmin) / self.hauteur + self.xmin)
             for y in range(self.largeur):
-                #In the same way, for each column of the matrix, we define cy, that will be used to change the values of (yn)
                 cy = (y * (self.ymin - self.ymax) / self.largeur + self.ymax)
-                #The first vakues of (xn) and (yn) are zeros. We start the program at time 0, so we are at the ieration 0 (n referes to the number of iterations already made)
                 xn, yn, n = 0, 0, 0
-                #The user has chosen the number of maximal iteration. While this number si not reached, every value of the matrix can be changed: if the squared norm of the vector (xn,yn) is less than 4, we change the series (xn) and (yn) without changing the value at the coordinate [x,y] of the matrix (the point at the coordinate (x,y) is in the Mandelbrot set): 
                 while (xn**2 + yn**2) < 4 and n < self.max_iteration:
                     tmp_x, tmp_y = xn, yn
                     xn = tmp_x**2 - tmp_y**2 + cx
                     yn = 2 * tmp_x * tmp_y + cy
                     n = n + 1
-                #If the square norm is higher than 4 and the number of maximal iterations is not reached, we change the value at the coordinate (x,y) of the matrix : the 0 becomes a 1 (this points is no longer is the Mandelbrot set but in is complementary
                 if n < self.max_iteration:
                     self.array[y, x] = 1
-        #When maxiteration is reached, we return the modifief matrix:
         return self.array
 
 
