c0cf62da60a7ed48a0996e216a22eab4e2b814e4
Cindy-dotcom1 <60815796+Cindy-dotcom1@users.noreply.github.com>
2020-04-28 19:47:00 +0200

    fix error

--- logistic_module/Mandelbrot2D/Mandelbrot_without_numba/Plot_pattern_without_numba.py
+++ logistic_module/Mandelbrot2D/Mandelbrot_without_numba/Plot_pattern_without_numba.py
@@ -14,19 +14,19 @@ def plot_patterns(x):
         Z_2=Mandelbrot_2D(500,500,600,-0.718,-0.714,0.213,0.217)
         fig = plt.figure()
         fig.suptitle("Minis mandelbrot set")
-        im = plt.imshow(Z_2.Mandelbrotset, cmap='binary')
+        im = plt.imshow(Z_2.Mandelbrotset(), cmap='binary')
         return im
     if x=="Elephant valley":
         Z_5=Mandelbrot_2D(500,500,600,0.26185,0.26196,0.002515,0.002573)
         fig = plt.figure()
         fig.suptitle("Elephant valleys")
-        im = plt.imshow(Z_5.Mandelbrotset, cmap='binary')
+        im = plt.imshow(Z_5.Mandelbrotset(), cmap='binary')
         return im
     if x=="Triple squared valley":
         Z=Mandelbrot_2D(500,500,600,-0.069,-0.0669,0.6478,0.6490)
         fig = plt.figure()
         fig.suptitle("Triple squared valley")
-        im = plt.imshow(Z.Mandelbrotset, cmap='binary')
+        im = plt.imshow(Z.Mandelbrotset(), cmap='binary')
         return im
     if x!="mini mandelbrot" and x!="Elephant valley" and x!="Triple squared valley":
         return "It seems like you did not chose one of the patterns of the list...Restart the function if you want to plot a characteristic pattern"
