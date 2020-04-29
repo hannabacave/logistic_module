453f083aa559a32deb8f9eb7b6db533be7b3388a
Cindy-dotcom1 <60815796+Cindy-dotcom1@users.noreply.github.com>
2020-04-29 20:32:58 +0200

    Update Inside_Mandelbrotset.py

--- logistic_module/Mandelbrot2D/Inside_Mandelbrotset.py
+++ logistic_module/Mandelbrot2D/Inside_Mandelbrotset.py
@@ -5,7 +5,7 @@ from matplotlib import animation
 from numba import jit 
 from memory_profiler import profile
 
-from logistic_module.Mandelbrot2D.Mandelbrot2D import Mandelbrot_2D
+from logistic_module.Mandelbrot2D import Mandelbrot_2D_jit
 
 @profile
 def Inside_the_set():
@@ -15,7 +15,7 @@ def Inside_the_set():
     """
     fig = plt.figure()
     fig.suptitle("Inside the Mandelbrot fractal")
-    Z=Mandelbrot_2D(500, 500, 50, -2, 0.5, -1.25, 1.25)
+    Z=Mandelbrot_2D_jit(500, 500, 50, -2, 0.5, -1.25, 1.25)
     im = plt.imshow(Z.Mandelbrotset, cmap='magma')
     #Possibility to save each frames in a temp directory, created on the fly
     x=input("Do you want to save each frame of the animation in a folder ? This will slow the animation. Answer 'yes' or 'no' without quotes :")
@@ -23,13 +23,13 @@ def Inside_the_set():
         os.mkdir('temp')
     def animate_200(i, x):
         if i<=50:
-            Z=Mandelbrot_2D(500,500,50,-2+0.02*i,0.5-0.02*i, -1.25+0.02*i, 1.25-0.02*i)
+            Z=Mandelbrot_2D_jit(500,500,50,-2+0.02*i,0.5-0.02*i, -1.25+0.02*i, 1.25-0.02*i)
             im.set_data(Z.Mandelbrotset)
         if i>50 and i<=130:
-            Z_1=Mandelbrot_2D(500,500,200,-1+0.0026*(i-50),-0.5-0.0021*(i-50), -0.25+0.0044*(i-50), 0.25-0.0003*(i-50))
+            Z_1=Mandelbrot_2D_jit(500,500,200,-1+0.0026*(i-50),-0.5-0.0021*(i-50), -0.25+0.0044*(i-50), 0.25-0.0003*(i-50))
             im.set_data(Z_1.Mandelbrotset)
         if i>130:
-            Z_2=Mandelbrot_2D(500,500,600,-0.74+0.00044*(i-150),-0.71-0.00008*(i-150), 0.19+0.00046*(i-150), 0.22-0.00006*(i-150))
+            Z_2=Mandelbrot_2D_jit(500,500,600,-0.74+0.00044*(i-150),-0.71-0.00008*(i-150), 0.19+0.00046*(i-150), 0.22-0.00006*(i-150))
             im.set_data(Z_2.Mandelbrotset)
         if x=='yes':
             plt.savefig('temp/frame_{}.png'.format(i))
