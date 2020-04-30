03d0455ab2cb73a1535cb3537139f1d13cfd3a2c
Cindy-dotcom1 <60815796+Cindy-dotcom1@users.noreply.github.com>
2020-04-30 17:43:19 +0200

    Update script.py

--- logistic_module/Mandelbrot2D/script.py
+++ logistic_module/Mandelbrot2D/script.py
@@ -45,7 +45,8 @@ end_2=time.time()
 print("Time spent to animate the zoom inside the Mandelbrotset:  {0:.5f} s.".format(end_2 - start_2))
 
 start_3=time.time()
-plot_patterns()
+x=input('Which one do you want to plot ("Elephant valley", "Triple squared valley" or "mini mandelbrot". Write the answer without quotes)?')
+plot_patterns(x)
 end_3=time.time()
 print("Time spent to plot a characteristic pattern:  {0:.5f} s.".format(end_3 - start_3))
 
