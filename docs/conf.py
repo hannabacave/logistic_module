2e090e7192c7ab0e527a4f521f00c85f2fbec76d
hannabacave <hannabacave@gmail.com>
2020-05-01 01:09:11 +0200

    fix error No module named sphinx_gallery

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
-
+import sphinx_gallery
 
 # -- Project information -----------------------------------------------------
 
