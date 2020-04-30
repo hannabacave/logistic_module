f6e6ebdcd21f27bb5c62824ad38f6db35f1cf37f
hannabacave <hannabacave@gmail.com>
2020-05-01 01:01:37 +0200

    tests to make gallery

---
+++ docs/auto_examples/plot_example.py
@@ -0,0 +1,11 @@
+"""
+Bifurcation diagram example
+=============================
+This example will show the bifurcation diagram
+"""
+
+import logistic_module
+
+visu = Visualization( r0=0.05, n=40, p=10000, ci=0.05, frames=82, threshold = 100)
+
+visu.bifurcation_diagram()
