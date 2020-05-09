da96cbca27b22a11f009aca56795f0a6cef3d31b
Cindy-dotcom1 <60815796+Cindy-dotcom1@users.noreply.github.com>
2020-05-09 18:04:33 +0200

    Changes for the doc

--- logistic_module/Mandelbrot2D/Mandelbrot2D.py
+++ logistic_module/Mandelbrot2D/Mandelbrot2D.py
@@ -15,9 +15,33 @@ spec = [('largeur', int64),
 class Mandelbrot_2D:
     """Returns a matrix with values 0 if the point is in the Mandelbrot set, 1 if it is not. The number of iterations can be chosen with maxiteration, so that the function return the state of the Mandelbrot set after this number of iterations
     
+    :param largeur: numbers of columns of the matrix
+    :type largeur: integer    
+
+    :param hauteur: numbers of lines of the matrix
+    :type hauteur: integer
+
+    :param max_iteration: numbers of iterations to make before returning the matrix
+    :type max_iteration: positive integer
+
+    :param xmin: minimum value for x
+    :type xmin: float
+
+    :param xmax: maximum value for x
+    :type xmax: float
+
+    :param ymin: minimum value for y
+    :type ymin: float
+
+    :param ymax: maximum value for y
+    :type ymax: float
+
     :return: A matrix with values 1 (if the point is not in the mandelbrot set) and 0 (if the point is in the Mandlebrot set)
     :rtype: array of integers
+
+        
     """
+        
     def __init__(self, largeur, hauteur, max_iteration, xmin, xmax, ymin, ymax):
         """Initialization of the parameters
 
@@ -52,20 +76,21 @@ class Mandelbrot_2D:
 
     @property
     def fig(self):
-        """ returns the matrix at the iteration 0
+        """ returns the matrix at the iteration 0:
+
             :return: a matrix with zeros
-            :return type: array of integers
-        """
+            :rtype: array of integers"""
+
         self.array = np.zeros((self.hauteur, self.largeur), dtype=np.int64)
         return self.array
     
     @property
     def Mandelbrotset(self):
-        """returns a matrix of the Mandelbrot set (with zeros and ones) that can be plotted
-        
+        """returns a matrix of the Mandelbrot set (with zeros and ones) that can be plotted:
+
            :return: the state of the Mandelbrot set at the iteration max_iteration
-           :return type:array of integers 
-        """
+           :rtype:array of integers"""
+
         self.array=self.fig
         for x in range(self.hauteur):
             cx = (x * (self.xmax - self.xmin) / self.hauteur + self.xmin)
@@ -80,10 +105,3 @@ class Mandelbrot_2D:
                 if n < self.max_iteration:
                     self.array[y, x] = 1
         return self.array
-
-
-
-
-
-
-
