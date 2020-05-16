e0e1a18a92fb1dcc6ebd8b9912537da6fd9739bb
hannabacave <hannabacave@gmail.com>
2020-05-16 21:43:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='logistic_module',
-  version= '0.0.2',
+  version= '1.0.1',
   description='Visualization of Mandelbrot fractal in 3D',
   url='https://github.com/hannabacave/Logistic_module',
   author='Cindy Delage, Hanna Bacave, Yassine Mimouni',
